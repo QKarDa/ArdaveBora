aff98b8995069878d13b89bc2645e6a2fe72d8e2
QKarDa <110054019+QKarDa@users.noreply.github.com>
2022-10-09 17:00:44 +0300

    Create AdoptMeFreeFR.lua

---
+++ AdoptMeFreeFR.lua
@@ -0,0 +1,11 @@
+local clientData = require(game.ReplicatedStorage.ClientModules.Core.ClientData)
+local playerData = clientData.get_data()[tostring(game.Players.LocalPlayer)]
+for i,v in pairs(playerData.inventory.pets) do
+v.properties.rideable = true
+v.properties.flyable = true
+v.properties.mega_neon = true
+end
+game.StarterGui:SetCore("SendNotification", {
+Title = "😈";
+Text = "Arda'nın Hilesini Kullandığınız İçin Teşekkürler ";
+})
