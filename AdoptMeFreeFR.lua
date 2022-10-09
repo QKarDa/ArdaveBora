3e6c2e53bc79db9b82ef247202c2bbbed40e6721
QKarDa <110054019+QKarDa@users.noreply.github.com>
2022-10-09 17:04:08 +0300

    Update AdoptMeFreeFR.lua

--- AdoptMeFreeFR.lua
+++ AdoptMeFreeFR.lua
@@ -3,7 +3,6 @@ local playerData = clientData.get_data()[tostring(game.Players.LocalPlayer)]
 for i,v in pairs(playerData.inventory.pets) do
 v.properties.rideable = true
 v.properties.flyable = true
-v.properties.mega_neon = true
 end
 game.StarterGui:SetCore("SendNotification", {
 Title = "😈";
